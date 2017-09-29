023645c0de229740dd323d9fbf70719b997a24eb
Quang C. Huynh <qhuynh@vims.edu>
2017-09-28 23:30:43 -0400

    Mean length-effort model does not estimate M.

--- functions/ML_functions.cpp
+++ functions/ML_functions.cpp
@@ -26,14 +26,14 @@ double GHeq_LL(double Z, NumericVector Lbar, NumericVector ss,
 }
 
 // [[Rcpp::export]]
-double GH_LL(NumericVector stpar, NumericVector year, NumericVector Lbar, NumericVector ss,
+double GH_LL(NumericVector stpar, NumericVector Lbar, NumericVector ss,
                   double Linf, double K, double Lc, int nbreaks) {
   
   int i;
   int j;
   int m;
   
-  int count = year.size();
+  int count = Lbar.size();
   int nbr = nbreaks-1;
   
   NumericVector Z(nbreaks+1);
@@ -129,6 +129,55 @@ double GHeffort(NumericVector stpar, NumericVector Lbar, NumericVector ss,
   
   double q = stpar[0];
   double M = stpar[1];
+  
+  double Z_init = q * eff_init + M;
+  NumericVector Z(n_yr);
+  NumericMatrix N(n_yr,n_age);
+  NumericVector La(n_age);
+  NumericVector age(n_age);
+  
+  NumericVector Lpred(n_yr);
+  double sum_square = 0.;
+  double sigma;
+  double nLL;
+  
+  for(a=0;a<n_age;a++) {
+    double ageD = a;
+    age[a] = ac + ageD;
+    La[a] = Linf * (1 - exp(-K*(age[a] - a0)));
+  }
+  for(y=0;y<n_yr;y++) Z(y) = q * eff(y) + M;
+  
+  N(0,0) = 1.;
+  for(a=1;a<n_age;a++) N(0,a) = N(0,a-1) * exp(-Z_init);
+  for(y=1;y<n_yr;y++) {
+    N(y,0) = 1.;
+    for(a=1;a<n_age;a++) N(y,a) = N(y-1,a-1) * exp(-Z(y-1));
+    Lpred(y) = sum(N(y,_)*La)/sum(N(y,_));
+    if(ss[y]>0) {
+      ndata += 1.;
+      sum_square += ss[y] * pow(Lbar[y] - Lpred[y], 2);
+    }
+  }
+  sigma = pow(sum_square/ndata, 0.5);
+  nLL = -ndata * log(sigma) - 0.5 * sum_square/(sigma*sigma);
+  nLL *= -1;
+  return nLL;
+}
+
+
+
+// [[Rcpp::export]]
+double GHeffort_fixM(double q, double M, NumericVector Lbar, NumericVector ss, 
+                NumericVector eff, double Linf, double K, double a0, 
+                double Lc, double eff_init, int n_age) {
+  
+  double ac = a0 - log(1 - Lc/Linf)/K;
+  
+  int n_yr = Lbar.size();
+  int y;
+  int a;
+  double ndata = 0.;
   
   double Z_init = q * eff_init + M;
   NumericVector Z(n_yr);
