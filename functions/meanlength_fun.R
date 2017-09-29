4c488a129b396abeb61c2ef47e7b6d7707377b43
Quang C. Huynh <qhuynh@vims.edu>
2017-09-28 23:31:34 -0400

    Limit Gedamke-Hoenig to most recent 40 years of data.

--- functions/meanlength_fun.R
+++ functions/meanlength_fun.R
@@ -32,10 +32,11 @@ model_select_GH <- function(mlen, ss = rep(1, length(mlen)), K, Linf, Lc) {
   mlen[mlen <= 0 | is.na(mlen)] <- -99
   ss[mlen == -99] <- 0
   
-  #if(length(mlen) > 40) {
-  #  mlen <- mlen[(length(mlen)-39):length(mlen)]
-  #  ss <- rep(1, 40)
-  #}
+  if(length(mlen) > 40) { # Use the most recent 40 years of data
+    yrind <- (length(mlen)-39):length(mlen)
+    mlen <- mlen[yrind]
+    ss <- ss[yrind]
+  }
   
   stpar <- 0.5
   res0 <- try(optim(stpar, GHeq_LL, method = "L-BFGS-B", lower = 0, 
@@ -45,38 +46,29 @@ model_select_GH <- function(mlen, ss = rep(1, length(mlen)), K, Linf, Lc) {
   if(inherits(res0, 'try-error')) aic0 <- NA
   else aic0 <- 2 * (res0$value + 2)
   
-  stpar <- c(0.5, length(mlen)/2)
-  res1 <- try(optim(stpar, GH_LL, method = "L-BFGS-B", lower = c(rep(0, 2), -Inf),
+  stpar <- c(rep(0.5, 2), length(mlen)/2)
+  res1 <- try(optim(stpar, GH_LL, method = "BFGS", 
                     Lbar = mlen, ss = ss, nbreaks = 1, 
                     K = K, Linf = Linf, Lc = Lc, control = list(maxit = 1e+06), 
                     hessian = FALSE), silent = TRUE)
   if(inherits(res1, 'try-error')) aic1 <- NA
-  else {
-    if(res1$convergence == 0) aic1 <- 2 * (res1$value + 4)
-    else aic1 <- NA
-  }
+  else aic1 <- 2 * (res1$value + 4)
   
-  stpar <- c(rep(0.5, 2), c(1, 2) * length(mlen)/3)
-  res2 <- try(optim(stpar, GH_LL, method = "L-BFGS-B", lower = c(rep(0, 3), rep(-Inf, 2)), 
+  stpar <- c(rep(0.5, 3), c(1, 2) * length(mlen)/3)
+  res2 <- try(optim(stpar, GH_LL, method = "BFGS", 
                     Lbar = mlen, ss = ss, nbreaks = 2, 
                     K = K, Linf = Linf, Lc = Lc, control = list(maxit = 1e+06), 
                     hessian = FALSE), silent = TRUE)
   if(inherits(res2, 'try-error')) aic2 <- NA
-  else {
-    if(res2$convergence == 0) aic2 <- 2 * (res1$value + 6)
-    else aic2 <- NA
-  }
+  else aic2 <- 2 * (res1$value + 6)
   
-  stpar <- c(rep(0.5, 3), c(1, 2, 3) * length(mlen)/4)
-  res3 <- try(optim(stpar, GH_LL, method = "L-BFGS-B", lower = c(rep(0, 4), rep(-Inf, 3)), 
+  stpar <- c(rep(0.5, 4), c(1, 2, 3) * length(mlen)/4)
+  res3 <- try(optim(stpar, GH_LL, method = "BFGS",
                     Lbar = mlen, ss = ss, nbreaks = 3, 
                     K = K, Linf = Linf, Lc = Lc, control = list(maxit = 1e+06), 
                     hessian = FALSE), silent = TRUE)
   if(inherits(res3, 'try-error')) aic3 <- NA
-  else {
-    if(res3$convergence == 0) aic3 <- 2 * (res3$value + 8)
-    else aic3 <- NA
-  }
+  else aic3 <- 2 * (res3$value + 8)
   
   testvec <- c(aic0, aic1, aic2, aic3)
   if(all(is.na(testvec))) Z.ans <- NA
@@ -84,12 +76,13 @@ model_select_GH <- function(mlen, ss = rep(1, length(mlen)), K, Linf, Lc) {
     index.best.nbreak <- which.min(testvec) # Find the model with the minimum AIC
     if(index.best.nbreak > 1) {
       # Choose model with n-1 change points instead of n change points if the difference in AIC is less than 2.
-      while(testvec[index.best.nbreak]-2 < testvec[index.best.nbreak-1] & index.best.nbreak > 1) {
+      if((testvec[index.best.nbreak]+2) > testvec[index.best.nbreak-1]) {
         index.best.nbreak <- index.best.nbreak-1 
       }
     }
     best.model <- get(paste0('res', index.best.nbreak - 1))
     Z.ans <- best.model$par[index.best.nbreak]
+    if(Z.ans < 0) Z.ans <- NA
   }
   return(Z.ans)
 }
